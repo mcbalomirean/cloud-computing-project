166de34592c5792aa66e4999d83168d8febd8d99
Mircea-Cristian Balomirean <mcbalomirean@gmail.com>
2022-05-15 00:42:48 +0300

    Refactoring and setup

---
+++ frontend/src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function MainPage() {
+  return (
+    <div id="MainPage">
+      <header className="h-14 bg-lime-700 flex justify-center">
+        <span className="self-center text-white text-bold text-xl">
+          Mail Translator
+        </span>
+      </header>
+    </div>
+  );
+}
