8249386c4fc47fbcb1777bd3aa2bc802a009a88e
Mircea-Cristian Balomirean <mcbalomirean@gmail.com>
2022-05-15 15:49:14 +0300

    Updates stuff

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,6 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import fs from "fs/promises";
+await fs.writeFile(
+  "./config/credentials.json",
+  process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON
+);
+
 export const applicationPort = process.env.PORT;
 
 export const databaseConfig = {
